7c1494c7094e85e1a2dc7920c8680e06f75ae1cc
Pierre-Luc BLOT <pierre-luc.blot@gmail.com>
2014-03-23 01:56:46 +0100

    Mise à jour de cellsGame

--- cellsGame/ui/main.js
+++ cellsGame/ui/main.js
@@ -12,6 +12,24 @@ lors de chaque appel de méthode sur les éléments de l'ui. */
 			 $( this ).trigger( 'valueChanged' );
 		}		
 	};
+
+	$.fn.updateRegen = function( v ){
+		
+		// si l'élément est une barre de niveau
+		if ( $( this ).attr( 'class' ) == 'property' ){
+			 $( this ).attr( 'regen', v );
+			 $( this ).trigger( 'regenChanged' );
+		}		
+	};
+
+	$.fn.updateMaxValue = function( v ){
+		
+		// si l'élément est une barre de niveau
+		if ( $( this ).attr( 'class' ) == 'property' ){
+			 $( this ).attr( 'max-value', v );
+			 $( this ).trigger( 'valueChanged' );
+		}		
+	};
 })( jQuery )
 
 
@@ -30,18 +48,38 @@ $( document ).ready( function(){
 
 	// construction des barres de niveau, par exemple pour l'énergie
 	$( '.property' ).each( function(){
+		
 		var value = parseInt( $( this ).attr( 'value' ) ),
-			caption = $( this ).attr( 'caption' );
+			maxValue = parseInt( $( this ).attr( 'max-value' ) ),
+			caption = $( this ).attr( 'caption' ),
+			regen = $( this ).attr( 'regen' );
 		$( this ).append( '<div id="caption">' + caption + '</div>');
-		$( this ).append( '<div id="levelQty"><div id="value"></div></div>');
+		if ( regen ) {
+			$( this ).append( '<div id="regen"></div>' );
+		}
+		$( this ).append( '<div id="levelQty"><div id="currency"></div><div id="value"></div></div>');
 
-		// création de l'écouteur sur l'élement
+		// création de l'écouteur sur l'élement pour le changement de value
 		$( this ).on( 'valueChanged', function(){
-			var value = parseInt( $( this ).attr( 'value' ) );
-			$( this ).find( '#value' ).css( 'width', value + '%' );
+			var value = parseInt( $( this ).attr( 'value' ) ),
+			    maxValue = parseInt( $( this ).attr( 'max-value' ) );
+			$( this ).find( '#value' ).css( 'width', ( value * 100 / maxValue ) + '%' );
 			$( this ).find( '#value' ).attr( 'level', levelColoration( value ) );
+			$( this ).find( '#currency' ).html( value + ' / ' + maxValue );
 		});
-
+		// initialisation de value
 		$( this ).updateValue( value );
+
+		/**
+		 * si la barre d'énergie possède un champs regen, alors
+		 * création de l'écouteur associé au changement de valeur de cet attribut
+		 */
+		if ( regen ) {
+			$( this ).on( 'regenChanged', function(){
+				var regen = $( this ).attr( 'regen' );
+				$( this ).find( '#regen' ).html( '+1 / ' + regen + 's' );
+			});
+			$( this ).updateRegen( regen );
+		}
 	} );
 } );
